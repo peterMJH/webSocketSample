50404a64299c7fd32029e1de3dcb2e9daefa5710
unknown <peter@fasoo.com>
2019-10-16 09:54:52 +0900

    initial commit

---
+++ backEnd/src/main/java/com/websocket/sample/WebSocketSampleApplication.java
@@ -0,0 +1,13 @@
+package com.websocket.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketSampleApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/test/java/com/websocket/sample/WebSocketSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
